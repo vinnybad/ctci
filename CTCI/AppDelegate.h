f960564f8b3fd1224ac73646ef14951cce4f78f1
Vinayak Ram <vinnybad@gmail.com>
2016-03-10 20:04:38 -0600

    Initial Commit

---
+++ CTCI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CTCI
+//
+//  Created by Vinayak Badrinathan on 3/10/16.
+//  Copyright © 2016 Vinayak Badrinathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
